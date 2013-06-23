8292f4ad39c7bd260d4122a2d30ce42dec6c2936
Mavis Ou <mmmavisss@gmail.com>
2013-06-22 23:22:57 -0700

    fixed typo

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.configure(function(){
 });
 
 if (process.env.REDISTOGO_URL) {
-    var rtg = require("url").parse(process.env.REDISTOGO_URL);
+    var rtg = require("url").parse(process.env.REDISCLOUD_URL);
     client = redis.createClient(rtg.port, rtg.hostname);
     redis.auth(rtg.auth.split(":")[1]);
 } else {
